46781a583c68b5668de8ae1ff5f1064f4a346fbc
jaomanoel <89238095+jaomanoel@users.noreply.github.com>
2023-04-14 02:21:16 -0300

    Created page of Terms of use

---
+++ src/pages/terms-of-use.tsx
@@ -0,0 +1,330 @@
+import { Head } from "@/infra/components/Head";
+import React from "react";
+
+const TermsOfUse = () => {
+  return (
+    <>
+      <Head title="Terms Of Use" />
+
+      <main className="px-16 font-montserrat text-base lg:text-lg">
+        <h4 className="font-bold text-3xl pb-32"> Termos e Condições </h4>{" "}
+        <p className="text-2xl pb-12"> Bem-vindo a Digital Business! </p>{" "}
+        <p>
+          {" "}
+          Estes termos e condições descrevem as regras e regulamentos para o uso
+          do site de Digital Business, localizado em
+          https://digital-business.jaomanoel.com/.{" "}
+        </p>{" "}
+        <p>
+          {" "}
+          Ao aceder a este site, presumimos que aceita estes termos e condições.
+          Não continue a usar Digital Business se não concordar com todos os
+          termos e condições declarados nesta página.{" "}
+        </p>{" "}
+        <h4 className="font-bold py-20 text-xl"> Cookies: </h4>{" "}
+        <p>
+          {" "}
+          O site usa cookies para ajudar a personalizar a sua experiência
+          online. Ao aceder Digital Business, concordou em usar os cookies
+          necessários.{" "}
+        </p>{" "}
+        <p>
+          {" "}
+          Um cookie é um arquivo de texto colocado no seu disco rígido por um
+          servidor de páginas web. Os cookies não podem ser usados para executar
+          programas ou enviar vírus para o seu computador. Os cookies são
+          atribuídos exclusivamente a si e só podem ser lidos por um servidor
+          web no domínio que emitiu o cookie.{" "}
+        </p>{" "}
+        <p>
+          {" "}
+          Podemos usar cookies para recolher, armazenar e rastrear informações
+          para fins estatísticos ou de marketing para operar o nosso site. Tem a
+          opção de aceitar ou recusar Cookies opcionais. Existem alguns Cookies
+          necessários para o funcionamento do nosso site. Esses cookies não
+          exigem o seu consentimento. Lembre-se de que, ao aceitar os cookies
+          necessários, também aceita cookies de terceiros, que podem ser usados
+          por meio de serviços fornecidos por terceiros se usar esses serviços
+          no nosso site, por exemplo, uma janela de exibição de vídeo fornecida
+          por terceiros e integrada no nosso site.{" "}
+        </p>{" "}
+        <h4 className="font-bold py-20 text-xl"> Licença: </h4>{" "}
+        <p>
+          {" "}
+          Salvo indicação em contrário, Digital Business e/ou os seus
+          licenciados possuem os direitos de propriedade intelectual de todo o
+          material em Digital Business. Todos os direitos de propriedade
+          intelectual são reservados. Pode aceder de Digital Business para o seu
+          uso pessoal sujeito às restrições definidas nestes termos e condições.{" "}
+        </p>{" "}
+        <p className="font-bold py-20 text-xl"> Não deve: </p>{" "}
+        <ul>
+          {" "}
+          <li> Copiar ou republicar o material de Digital Business </li>{" "}
+          <li>
+            {" "}
+            Vender, alugar ou sublicenciar o material de Digital Business{" "}
+          </li>{" "}
+          <li>
+            {" "}
+            Reproduzir, duplicar ou copiar o material de Digital Business{" "}
+          </li>{" "}
+          <li> Redistribuir o conteúdo de Digital Business </li>{" "}
+        </ul>{" "}
+        <p> Este acordo deve iniciar na data deste documento. </p>{" "}
+        <p>
+          {" "}
+          Partes deste site oferecem aos utilizadores a oportunidade de postar e
+          trocar opiniões e informações em certas áreas do site. Digital
+          Business não filtra, edita, publica ou analisa os comentários no site.
+          Os comentários não refletem as visões e opiniões de Digital Business,
+          seus agentes e/ou afiliados. Os comentários refletem os pontos de
+          vista e as opiniões da pessoa que posta os seus pontos de vista e
+          opiniões. Na medida do permitido pelas leis aplicáveis, Digital
+          Business não será responsável pelos comentários ou qualquer
+          responsabilidade, danos ou despesas causados e/ou sofridos como
+          resultado de qualquer uso e/ou publicação e/ou aparência de
+          comentários neste site.{" "}
+        </p>{" "}
+        <p>
+          {" "}
+          Digital Business reserva-se o direito de monitorizar todos os
+          comentários e remover quaisquer comentários que possam ser
+          considerados inadequados, ofensivos ou que causem violação destes
+          Termos e Condições.{" "}
+        </p>{" "}
+        <p className="font-bold py-20 text-xl"> Garante e declara que: </p>{" "}
+        <ul>
+          {" "}
+          <li>
+            {" "}
+            tem o direito de postar os comentários no nosso site e tem todas as
+            licenças e consentimentos necessários para fazê-lo;{" "}
+          </li>{" "}
+          <li>
+            {" "}
+            os comentários não invadem nenhum direito de propriedade
+            intelectual, incluindo, sem limitação, direitos de autor, patentes
+            ou marcas comerciais de terceiros;{" "}
+          </li>{" "}
+          <li>
+            {" "}
+            os comentários não contêm nenhum material difamatório, calunioso,
+            ofensivo, indecente ou de outra forma ilegal, o que constitui uma
+            invasão de privacidade.{" "}
+          </li>{" "}
+          <li>
+            {" "}
+            os comentários não serão usados para solicitar ou promover negócios
+            ou customizar ou apresentar atividades comerciais ou atividades
+            ilegais.{" "}
+          </li>{" "}
+        </ul>{" "}
+        <p>
+          Concede a Digital Business uma licença não exclusiva para usar,
+          reproduzir, editar e autorizar outros a usar, reproduzir e editar
+          qualquer um de seus comentários em todas e quaisquer formas, formatos,
+          ou mídia.{" "}
+        </p>{" "}
+        <h4> Hiperlinks para o nosso conteúdo: </h4>{" "}
+        <p>
+          {" "}
+          As seguintes organizações podem vincular-se ao nosso site sem
+          aprovação prévia por escrito:{" "}
+        </p>{" "}
+        <ul>
+          {" "}
+          <li> Agências governamentais; </li> <li> Mecanismos de pesquisa; </li>{" "}
+          <li> Organizações de notícias; </li>{" "}
+          <li>
+            {" "}
+            Os distribuidores de diretórios online podem ter links para o nosso
+            site da mesma maneira que fazem um hiperlink para sites de outras
+            empresas listadas; e{" "}
+          </li>{" "}
+          <li>
+            {" "}
+            Negócios credenciados em todo o sistema, exceto a solicitação de
+            organizações sem fins lucrativos, centros de caridade e grupos de
+            angariação de fundos de caridade que não podem ter um link para o
+            nosso site.{" "}
+          </li>{" "}
+        </ul>{" "}
+        <p>
+          {" "}
+          Essas organizações podem incluir links para a nossa página inicial,
+          publicações ou outras informações do site, desde que o link: (a) não
+          seja de forma alguma enganoso; (b) não implique falsamente um
+          patrocínio, endosso ou aprovação da parte vinculante e de seus
+          produtos e/ou serviços; e (c) se enquadra no contexto do site da parte
+          vinculante.{" "}
+        </p>{" "}
+        <p>
+          {" "}
+          Podemos considerar e aprovar outras solicitações de links dos
+          seguintes tipos de organizações:{" "}
+        </p>{" "}
+        <ul>
+          {" "}
+          <li>
+            {" "}
+            fontes de informações comerciais e / ou de consumidores comumente
+            conhecidas;{" "}
+          </li>{" "}
+          <li> sites da comunidade dot.com; </li>{" "}
+          <li>
+            {" "}
+            associações ou outros grupos que representam instituições de
+            caridade;{" "}
+          </li>{" "}
+          <li> distribuidores de diretórios online; </li>{" "}
+          <li> portais de internet; </li>{" "}
+          <li> firmas de contabilidade, advocacia e consultoria; e </li>{" "}
+          <li> instituições educacionais e associações comerciais. </li>{" "}
+        </ul>{" "}
+        <p>
+          {" "}
+          Aprovaremos as solicitações de link dessas organizações se decidirmos
+          que: (a) o link não nos faria parecer desfavoravelmente para nós
+          mesmos ou para nossos negócios credenciados; (b) a organização não
+          possui registros negativos conosco; (c) o benefício para nós da
+          visibilidade do hiperlink compensa a ausência de Digital Business; e
+          (d) o link está no contexto de informações gerais de recursos.{" "}
+        </p>{" "}
+        <p>
+          {" "}
+          Essas organizações podem ter um link para nossa página inicial, desde
+          que o link: (a) não seja de forma alguma enganoso; (b) não implique
+          falsamente em patrocínio, endosso ou aprovação da parte vinculante e
+          de seus produtos ou serviços; e (c) se enquadra no contexto do site da
+          parte vinculante.{" "}
+        </p>{" "}
+        <p>
+          {" "}
+          Se é uma das organizações listadas no parágrafo 2 acima e está
+          interessado em criar um link para o nosso site, deve-nos informar
+          enviando um email para Digital Business. Inclua o seu nome, o nome da
+          sua organização, informações de contato, bem como o URL do seu site,
+          uma lista de todos os URLs dos quais pretende criar um link para o
+          nosso site e uma lista dos URLs do nosso site aos quais gostaria de
+          linkar. Espere 2 a 3 semanas por uma resposta.{" "}
+        </p>{" "}
+        <p>
+          {" "}
+          Organizações aprovadas podem ter um hiperlink para nosso site da
+          seguinte forma:{" "}
+        </p>{" "}
+        <ul>
+          {" "}
+          <li> Pelo uso do nosso nome corporativo; ou </li>{" "}
+          <li>
+            {" "}
+            Pelo uso do localizador uniforme de recursos ao qual está vinculado;
+            ou{" "}
+          </li>{" "}
+          <li>
+            {" "}
+            Usando qualquer outra descrição do nosso site vinculado que faça
+            sentido dentro do contexto e formato do conteúdo do site da parte
+            vinculante.{" "}
+          </li>{" "}
+        </ul>{" "}
+        <p>
+          {" "}
+          Não será permitido o uso do logotipo da Digital Business ou outra arte
+          para criar links na ausência de um contrato de licença de marca
+          registada.{" "}
+        </p>{" "}
+        <h4 className="font-bold py-20 text-xl">
+          {" "}
+          Responsabilidade pelo conteúdo:{" "}
+        </h4>{" "}
+        <p>
+          {" "}
+          Não seremos responsabilizados por qualquer conteúdo que apareça no seu
+          site. Concorda em proteger-nos e defender contra todas as reclamações
+          levantadas no seu site. Nenhum link deve aparecer em qualquer site que
+          possa ser interpretado como calunioso, obsceno ou criminoso, ou que
+          infrinja, de outra forma viole ou defenda a violação ou outra violação
+          de quaisquer direitos de terceiros.{" "}
+        </p>{" "}
+        <h4 className="font-bold py-20 text-xl"> Reserva de direitos: </h4>{" "}
+        <p>
+          {" "}
+          Reservamos o direito de solicitar que remova todos os links ou
+          qualquer link específico para o nosso site. Aprova a remoção imediata
+          de todos os links para nosso site, mediante solicitação. Também nos
+          reservamos o direito de alterar estes termos e condições e a sua
+          política de vinculação a qualquer momento. Ao conectar-se
+          continuamente ao nosso site, concorda em cumprir e seguir estes termos
+          e condições de vinculação.{" "}
+        </p>{" "}
+        <h4> Remoção de links do nosso site: </h4>{" "}
+        <p>
+          {" "}
+          Se encontrar algum link no nosso site que seja ofensivo por qualquer
+          motivo, pode contactar-nos e informar-nos a qualquer momento.
+          Consideraremos as solicitações de remoção de links, mas não somos
+          obrigados a fazê-lo ou a responder diretamente.{" "}
+        </p>{" "}
+        <p>
+          {" "}
+          Não garantimos que as informações neste site sejam corretas. Não
+          garantimos sua integridade ou precisão, nem prometemos garantir que o
+          site permaneça disponível ou que o material nele contido seja mantido
+          atualizado.{" "}
+        </p>{" "}
+        <h4 className="font-bold py-20 text-xl">
+          {" "}
+          Isenção de responsabilidade:{" "}
+        </h4>{" "}
+        <p>
+          {" "}
+          Na extensão máxima permitida pela lei aplicável, excluímos todas as
+          representações, garantias e condições relacionadas ao nosso site e ao
+          uso deste site. Nada nesta isenção de responsabilidade:{" "}
+        </p>{" "}
+        <ul>
+          {" "}
+          <li>
+            {" "}
+            limitará ou excluirá a nossa responsabilidade por morte ou danos
+            pessoais;{" "}
+          </li>{" "}
+          <li>
+            {" "}
+            limitar ou excluir a nossa responsabilidade ou a sua
+            responsabilidade por fraude ou deturpação fraudulenta;{" "}
+          </li>{" "}
+          <li>
+            {" "}
+            limitar qualquer uma das nossas responsabilidades de qualquer forma
+            que não seja permitida pela lei aplicável; ou{" "}
+          </li>{" "}
+          <li>
+            {" "}
+            excluir qualquer uma das nossas ou suas responsabilidades que não
+            possam ser excluídas sob a lei aplicável.{" "}
+          </li>{" "}
+        </ul>{" "}
+        <p>
+          {" "}
+          As limitações e proibições de responsabilidade definidas nesta seção e
+          em outras partes desta isenção de responsabilidade: (a) estão sujeitas
+          ao parágrafo anterior; e (b) regem todas as responsabilidades
+          decorrentes da isenção de responsabilidade, incluindo
+          responsabilidades decorrentes de contratos, atos ilícitos e por
+          violação de dever estatutário.{" "}
+        </p>{" "}
+        <p>
+          {" "}
+          Desde que o site e as informações e serviços nele fornecidos sejam
+          gratuitos, não seremos responsáveis por perdas ou danos de qualquer
+          natureza.
+        </p>
+      </main>
+    </>
+  );
+};
+
+export default TermsOfUse;
