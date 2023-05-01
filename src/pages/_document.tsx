a50979502e55024346d72bb0fab9428046634a2e
jaomanoel <belezajovaniatb@gmail.com>
2023-05-01 09:28:25 -0300

    Added tag of goole analitics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,25 @@
 /* eslint-disable @next/next/inline-script-id */
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="pt-br">
       <Head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-2D0DMCPVNP"
+          strategy="afterInteractive"
+        ></Script>
+        <Script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-2D0DMCPVNP');
+          `}
+        </Script>
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
